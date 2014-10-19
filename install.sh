6ef31c45164769160b0201f0f71fc438d065e761
Marcel Morgan <marcel.morgan@codedry.com>
2014-10-18 22:14:44 -0500

    Add sample install.zsh file

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# install homebrew - http://brew.sh
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Rename system vim
+sudo mv /usr/bin/vim /usr/bin/vim-system
+
+# Base packages via vim
+brew install zsh tmux vim postgres rbenv ruby-build
+
+# install vimified
+
+# install ohmyzsh

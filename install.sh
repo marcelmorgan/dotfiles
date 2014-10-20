907c27c4d938259a4a954d7f43fab93f879bc2f9
Marcel Morgan <marcel.morgan@codedry.com>
2014-10-20 13:55:02 -0500

    Add packages required for ruby 1.8

--- install.sh
+++ install.sh
@@ -7,7 +7,11 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 sudo mv /usr/bin/vim /usr/bin/vim-system
 
 # Base packages via vim
-brew install zsh tmux vim postgres rbenv ruby-build nodejs phantomjs
+brew install zsh tmux vim postgres myqsql rbenv ruby-build nodejs qt
+npm install -g phantomjs
+
+
+brew tap homebrew/dupes ; brew install apple-gcc42 # for ruby 1.8 compilation
 
 # install vimified
 

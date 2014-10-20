a93a625200ad678699af3bdf7cd81087ba2249f3
Marcel Morgan <marcel.morgan@codedry.com>
2014-10-20 16:22:49 -0500

    Install the silver searcher (ag)

--- install.sh
+++ install.sh
@@ -7,9 +7,9 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 sudo mv /usr/bin/vim /usr/bin/vim-system
 
 # Base packages via vim
-brew install zsh tmux vim postgres myqsql rbenv ruby-build nodejs qt
-npm install -g phantomjs
+brew install zsh tmux vim postgres myqsql rbenv ruby-build nodejs the_silver_searcher qt
 
+npm install -g phantomjs
 
 brew tap homebrew/dupes ; brew install apple-gcc42 # for ruby 1.8 compilation
 
--- local.vimrc
+++ local.vimrc
@@ -1,1 +1,9 @@
 let g:vimified_packages = ['general', 'coding', 'ruby', 'js', 'html', 'css', 'color']
+
+let g:ctrlp_user_command = 'ag %s -i --nocolor --nogroup --hidden
+      \ --ignore .git
+      \ --ignore .svn
+      \ --ignore .hg
+      \ --ignore .DS_Store
+      \ --ignore "**/*.pyc"
+      \ -g ""'

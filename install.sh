56ffc7a246dc1a2de6405c0027bac5b2e687aaf3
Marlon Williams <marlon.willi@gmail.com>
2018-02-11 22:37:09 -0500

    Change zsh installation to use github.com/robbyrussell/oh-my-zsh instead of homebrew

--- install.sh
+++ install.sh
@@ -3,16 +3,17 @@
 # install homebrew - http://brew.sh
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+# install ohmyzsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 # Rename system vim
 sudo mv /usr/bin/vim /usr/bin/vim-system
 
 # Base packages via vim
-brew install git zsh tmux vim ssh-copy-id postgres mysql rbenv ruby-build nodejs the_silver_searcher qt unrar
+brew install git tmux vim ssh-copy-id postgres mysql rbenv ruby-build nodejs the_silver_searcher qt unrar
 
 npm install -g phantomjs
 
 brew tap homebrew/dupes ; brew install apple-gcc42 # for ruby 1.8 compilation
 
 # install vimified
-
-# install ohmyzsh
